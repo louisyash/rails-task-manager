95c5c2e0b54a008410b42c467e65cd4dd6abadb7
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-09 16:58:35 +0900

    done with functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,13 +9,42 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params[:title], params[:details], params[:completed])
-    @task.save
+    @task = Task.new(task_params)
+      if @task.save
+      redirect_to tasks_path(@task)
+    else
+      render 'new.html.erb'
+    end
   end
 
   def new
     @task = Task.new
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task[:title] = params[:task][:title]
+    @task[:details] = params[:task][:details]
+     @task[:completed] = params[:task][:completed]
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
+end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "tasks/new", to:"tasks#new", as: :new
-  post "/tasks", to: "tasks#create"
-  get "/tasks", to: "tasks#index", as: :tasks
-  get "/tasks/:id", to: "tasks#show", as: :task
+  # get "tasks/new", to:"tasks#new", as: :new_task
+  # post "/tasks", to: "tasks#create"
+  # get "/tasks", to: "tasks#index"
+  # get "/tasks/edit/:id", to: "tasks#edit", as: edit_task_path
+  # get "/tasks/:id", to: "tasks#show", as: :task
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
